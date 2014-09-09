d3e466a15ed8397e60f54387393608083e7096e1
Lixin Fan <900831flx@gmail.com>
2014-09-09 15:59:10 +0800

    update buld rule

--- cpp-utility.cc
+++ cpp-utility.cc
@@ -2,14 +2,6 @@
 #include <gtest/gtest.h>
 #include "cpp-utility-config.h"
 
-// #include "base/clock.h"
-// #include "strings/string_piece_test.h"
-// #include "strings/stringprintf_test.h"
-// #include "util/singleton_test.h"
-// #include "util/registerer_test.h"
-// #include "util/csv/csv_test.h"
-// #include "util/comparator_test.h"
-
 #include <cstdio>
 int main(int argc, char* argv[]) {
   printf("%d\n", CPP_UTILITY_VERSION_MAJOR);
